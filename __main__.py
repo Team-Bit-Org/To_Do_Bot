cc9cbf9b295c1faacb091032a0111d089510baf5
jjun4341 <42130391+jjun4341@users.noreply.github.com>
2021-06-24 23:27:36 +0900

    feat: add To do (40%)

--- __main__.py
+++ __main__.py
@@ -1,5 +1,10 @@
 from discord.ext import command
 import disord
+from pymongo import MongoClient
+import os
+
+coll = MongoClient("mongodb://localhost:27017").ToDo.user
+
 
 bot = commands.Bot(
     command_prefix="-",
@@ -11,3 +16,16 @@ bot = commands.Bot(
 async def on_ready():
     print(f"{bot.user} is Ready.")
 
+
+@bot.command(name="add")
+async def _todo_add(ctx, name=None, content=None):
+    if name == None:
+        embed=discord.Embed(
+            title="오류 발생", 
+            description='올바른 사용법 : `!add "To Do 이름" "To Do 설명"', 
+            color=discord.Colour.red()
+        )
+        await ctx.send(embed=embed)
+    elif os.path.isdir(f"./ToDo/{ctx.author.id}/"):
+        with open("./ToDo/{ctx.author.id}/{name}.txt", "w", encoding="UTF-8") as f:
+            f.write(str(content))
