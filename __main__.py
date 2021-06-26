accd3a7f88208c3c678f2e24a0b179bd27b7975b
jjun4341 <jjun4341@naver.com>
2021-06-26 23:57:58 +0900

    feat: done

--- __main__.py
+++ __main__.py
@@ -41,8 +41,8 @@ async def _todo_add(ctx, name=None, content=None):
         description="To Do 등록이 완료되었습니다.\n마감일 등록이 필요하시다면 아래 이모지를 눌러주세요.",
         color=discord.Colour.green()
     )
-    msg = await ctx.send(embed=e)
-    await msg.add_reaction("🗓")
+    mmm = await ctx.send(embed=e)
+    await mmm.add_reaction("🗓")
     
     def check(reaction, user):
         return user == ctx.author and str(reaction.emoji) == "🗓"
@@ -54,21 +54,190 @@ async def _todo_add(ctx, name=None, content=None):
         reaction, user = await bot.wait_for("reaction_add", timeout=60, check=check)
         embed = discord.Embed(
             title='작업 진행 중',
-            description='작업이 진행중입니다.\n\n마감일 등록   :   🟥\n마감 당일 안내  :  🟥\n작업 집중 메세지 : 🟥',
+            description='작업이 진행중입니다.\n\n마감일 등록     : 🟥\n마감 당일 안내   : 🟥\n작업 집중 메세지 : 🟥',
             color=discord.Colour.blurple()
         )
         embed.set_footer(text='⬛ - 작업 진행중 ㅣ 🟥 - 작업 미진행 ㅣ 🟩 - 작업 완료')
-        await ctx.send(embed=embed)
+        msg = await ctx.send(embed=embed)
+        embed = discord.Embed(
+            title='작업 진행 중',
+            description='작업이 진행중입니다.\n\n마감일 등록     : ⬛\n마감 당일 안내   : 🟥\n작업 집중 메세지 : 🟥',
+            color=discord.Colour.blurple()
+        )
+        embed.set_footer(text='⬛ - 작업 진행중 ㅣ 🟥 - 작업 미진행 ㅣ 🟩 - 작업 완료')
+        await msg.edit(embed=embed)
+        embed.set_footer(text='⬛ - 작업 진행중 ㅣ 🟥 - 작업 미진행 ㅣ 🟩 - 작업 완료')
         await asyncio.sleep(1)
         await ctx.send('마감 일을 `YYYY/MM/DD` 형식으로 입력해주세요.\n예시 : 2021/06/26')
 
         desc = await bot.wait_for("message", timeout=60, check=text_check)
-        desc = desc.content
+        date = desc.content
 
-        await ctx.send('마감 당일 안내를 활성화 하시겠습니까?')
+        embed = discord.Embed(
+            title='작업 진행 중',
+            description=f'작업이 진행중입니다.\n\n마감일 등록     : 🟩\n  - `{date}`\n마감 당일 안내   : 🟩\n작업 집중 메세지 : 🟥',
+            color=discord.Colour.blurple()
+        )
+        embed.set_footer(text='⬛ - 작업 진행중 ㅣ 🟥 - 작업 미진행 ㅣ 🟩 - 작업 완료')
+        await msg.edit(embed=embed)
+
+        msgs = await ctx.send('마감 당일 안내를 활성화 하시겠습니까?')
+        embed = discord.Embed(
+            title='작업 진행 중',
+            description=f'작업이 진행중입니다.\n\n마감일 등록     : 🟩\n  - `{date}`\n마감 당일 안내   : ⬛\n작업 집중 메세지 : 🟥',
+            color=discord.Colour.blurple()
+        )
+        embed.set_footer(text='⬛ - 작업 진행중 ㅣ 🟥 - 작업 미진행 ㅣ 🟩 - 작업 완료')
+        await msg.edit(embed=embed)
+
+        emoji = ['✅', '❎']
+        for i in emoji: await msgs.add_reaction(i)
+
+        def check1(reaction, user):
+            return user == ctx.author and str(reaction.emoji) in emoji
+
+        try:
+            reaction, user = await bot.wait_for("reaction_add", timeout=60, check=check1)
+            if str(reaction.emoji) == emoji[0]:  # 마감 당일 안내 활성화
+                with open('./alarm.txt', 'a', encoding="UTF-8") as f:
+                    f.write(str(date) + '\n')
+
+                embed = discord.Embed(
+                    title='작업 진행 중',
+                    description=f'작업이 진행중입니다.\n\n마감일 등록     : 🟩\n  - `{date}`\n마감 당일 안내   : 🟩\n  - 활성화\n작업 집중 메세지 : 🟥',
+                    color=discord.Colour.blurple()
+                )
+                embed.set_footer(text='⬛ - 작업 진행중 ㅣ 🟥 - 작업 미진행 ㅣ 🟩 - 작업 완료')
+                await msg.edit(embed=embed)
+                await ctx.send('마감 당일 안내를 활성화 하였습니다.')
+                await asyncio.sleep(0.5)
+                msgss = await ctx.send('작업 집중 메세지를 활성화 하시겠습니까?')
+                for i in emoji: await msgss.add_reaction(i)
+                embed = discord.Embed(
+                    title='작업 진행 중',
+                    description=f'작업이 진행중입니다.\n\n마감일 등록     : 🟩'
+                                f'\n  - `{date}`\n마감 당일 안내   : 🟩\n  - 활성화\n작업 집중 메세지 : ⬛',
+                    color=discord.Colour.blurple()
+                )
+                embed.set_footer(text='⬛ - 작업 진행중 ㅣ 🟥 - 작업 미진행 ㅣ 🟩 - 작업 완료')
+                await msg.edit(embed=embed)
+                try:
+                    reaction, user = await bot.wait_for("reaction_add", timeout=60, check=check1)
+                    if str(reaction.emoji) == emoji[0]:
+                        await ctx.send('알림 빈도를 5분 단위, 그리고 숫자만 입력해주세요.')
+                        descc = await bot.wait_for("message", timeout=60, check=text_check)
+                        time = descc.content
+
+                        embed = discord.Embed(
+                            title='작업 진행 중',
+                            description=f'작업이 진행중입니다.\n\n마감일 등록     : 🟩'
+                                        f'\n  - `{date}`\n마감 당일 안내   : 🟩\n  - 활성화\n작업 집중 메세지 : 🟩\n  - `{time}`분',
+                            color=discord.Colour.blurple()
+                        )
+                        embed.set_footer(text='⬛ - 작업 진행중 ㅣ 🟥 - 작업 미진행 ㅣ 🟩 - 작업 완료')
+                        await msg.edit(embed=embed)
+
+                        embed = discord.Embed(
+                            title='완료',
+                            description='To Do 등록을 최종적으로 완료하였습니다.',
+                            color=discord.Colour.green()
+                        )
+                        await mmm.edit(embed=embed)
+                        await asyncio.sleep(3)
+                        await msg.delete()
+                    else:
+                        await ctx.send('작업 집중 메세지를 비활성화 하였습니다.')
+                        embed = discord.Embed(
+                            title='작업 진행 중',
+                            description=f'작업이 진행중입니다.\n\n마감일 등록     : 🟩'
+                                        f'\n  - `{date}`\n마감 당일 안내   : 🟩\n  - 활성화\n작업 집중 메세지 : 🟩\n  - 비활성화',
+                            color=discord.Colour.blurple()
+                        )
+                        embed.set_footer(text='⬛ - 작업 진행중 ㅣ 🟥 - 작업 미진행 ㅣ 🟩 - 작업 완료')
+                        await msg.edit(embed=embed)
+
+                        embed = discord.Embed(
+                            title='완료',
+                            description='To Do 등록을 최종적으로 완료하였습니다.',
+                            color=discord.Colour.green()
+                        )
+                        await mmm.edit(embed=embed)
+                        await asyncio.sleep(3)
+                        await msg.delete()
+                except asyncio.TimeoutError:
+                    await ctx.send('시간이 초과되었습니다.')
+
+            else:  # 마감 당일 안내 비활성화
+                embed = discord.Embed(
+                    title='작업 진행 중',
+                    description=f'작업이 진행중입니다.\n\n마감일 등록     : 🟩\n  - `{date}`\n마감 당일 안내   : 🟩\n  - 비활성화\n작업 집중 메세지 : 🟥',
+                    color=discord.Colour.blurple()
+                )
+                embed.set_footer(text='⬛ - 작업 진행중 ㅣ 🟥 - 작업 미진행 ㅣ 🟩 - 작업 완료')
+                await msg.edit(embed=embed)
+                await ctx.send('마감 당일 안내를 비활성화 하였습니다.')
+
+                msgss = await ctx.send('작업 집중 메세지를 활성화 하시겠습니까?')
+                for i in emoji: await msgss.add_reaction(i)
+                embed = discord.Embed(
+                    title='작업 진행 중',
+                    description=f'작업이 진행중입니다.\n\n마감일 등록     : 🟩'
+                                f'\n  - `{date}`\n마감 당일 안내   : 🟩\n  - 비활성화\n작업 집중 메세지 : ⬛',
+                    color=discord.Colour.blurple()
+                )
+                embed.set_footer(text='⬛ - 작업 진행중 ㅣ 🟥 - 작업 미진행 ㅣ 🟩 - 작업 완료')
+                await msg.edit(embed=embed)
+                try:
+                    reaction, user = await bot.wait_for("reaction_add", timeout=60, check=check1)
+                    if str(reaction.emoji) == emoji[0]:  # 작업 집중 메세지 활성화
+                        await ctx.send('알림 빈도를 5분 단위, 그리고 숫자만 입력해주세요.')
+                        descc = await bot.wait_for("message", timeout=60, check=text_check)
+                        time = descc.content
+
+                        embed = discord.Embed(
+                            title='작업 진행 중',
+                            description=f'작업이 진행중입니다.\n\n마감일 등록     : 🟩'
+                                        f'\n  - `{date}`\n마감 당일 안내   : 🟩\n  - 비활성화\n작업 집중 메세지 : 🟩\n  - `{time}`분',
+                            color=discord.Colour.blurple()
+                        )
+                        embed.set_footer(text='⬛ - 작업 진행중 ㅣ 🟥 - 작업 미진행 ㅣ 🟩 - 작업 완료')
+                        await msg.edit(embed=embed)
+
+                        embed = discord.Embed(
+                            title='완료',
+                            description='To Do 등록을 최종적으로 완료하였습니다.',
+                            color=discord.Colour.green()
+                        )
+                        await mmm.edit(embed=embed)
+                        await asyncio.sleep(3)
+                        await msg.delete()
+                    else:  # 작업 집중 메세지 비활성화
+                        await ctx.send('작업 집중 메세지를 비활성화 하였습니다.')
+                        embed = discord.Embed(
+                            title='작업 진행 중',
+                            description=f'작업이 진행중입니다.\n\n마감일 등록     : 🟩'
+                                        f'\n  - `{date}`\n마감 당일 안내   : 🟩\n  - 비활성화\n작업 집중 메세지 : 🟩\n  - 비활성화',
+                            color=discord.Colour.blurple()
+                        )
+                        embed.set_footer(text='⬛ - 작업 진행중 ㅣ 🟥 - 작업 미진행 ㅣ 🟩 - 작업 완료')
+                        await msg.edit(embed=embed)
+
+                        embed = discord.Embed(
+                            title='완료',
+                            description='To Do 등록을 최종적으로 완료하였습니다.',
+                            color=discord.Colour.green()
+                        )
+                        await mmm.edit(embed=embed)
+                        await asyncio.sleep(3)
+                        await msg.delete()
+
+                except asyncio.TimeoutError:
+                    await ctx.send('시간이 초과되었습니다.')
+        except asyncio.TimeoutError:
+            await ctx.send('시간이 초과되었습니다.')
     except asyncio.TimeoutError:
         await ctx.send("시간이 초과되었습니다.")
 
 
-bot.run('ODUxMDkyOTEzNTM3NTQ4MzQ4.YLzQCw.id8BcyBbUFcYUdtcGvvDBWH8QqU')
+bot.run('ODUxMDkyOTEzNTM3NTQ4MzQ4.YLzQCw.R-mbTy9kiGCqYZAD_1rmKY4K8Sw')
 
